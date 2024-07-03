73afb4ae315d4a45c1cdbfecda6b5a5cebdbb392
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2024-07-03 06:43:32 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Search from "./Components/Search";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="container">
         <h1>Weather with Sense Power Digital</h1>
         <Search />
       </div>
